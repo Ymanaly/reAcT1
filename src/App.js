1a1b9e373f25c9e5f8aa28c6540ee1070cd7ccff
Ymanaly <old.vai2004@gmail.com>
2022-12-27 16:51:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({
+      name: '',
+      degree: 0,
+      humidity: 0,
+      weather: '',
+      feels_like: '',
+      wind: 0
+    })
+    const [location, setLocation] = useState("Bishkek");
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=944124f32a772610b9043e15780504e8`;
+
+    useEffect(() => {
+      axios.get(url).then((response) => {
+        setData({
+          name: response.data.name,
+          degree: response.data.main.temp.toFixed(),
+          humidity: response.data.main.humidity.toFixed(),
+          weather: response.data.weather[0].description,
+          feels_like: response.data.main.feels_like,
+          wind: response.data.wind.speed.toFixed()
+        });
+        console.log(response.data);
+      });
+    }, [])
+
+    useEffect(() => {
+      console.log(data);
+    }, [data])
+
+    const seacrhLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+              setData({
+                name: response.data.name,
+                degree: response.data.main.temp.toFixed(),
+                humidity: response.data.main.humidity.toFixed(),
+                weather: response.data.weather[0].description,
+                feels_like: response.data.main.feels_like,
+                wind: response.data.wind.speed.toFixed()
+              });
+
+            });
+            setLocation("")
+          console.log(data);
+        }
+    };
+
+
+
+    return (
+      <div className="app">
+        <div className="search">
+          <input
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyPress={seacrhLocation}
+            placeholder="Введите название..."
+            type="text"
+          />
+        </div>
+          <div className="container">
+            <div className="top">
+              <div className="location">
+                <p>{data.name}</p>
+              </div>
+              <div className="temp">
+                <h1>{data.degree}</h1>
+              </div>
+              <div className="description">
+                <p>{data.weather}</p>
+              </div>
+            </div>
+            <div className="bottom">
+                <div className="feels ">
+                  <p className="bold">{`${data.feels_like}C`}</p>
+                  <p>Чувствуется как</p>
+                </div>
+                <div className="humidity">
+                  <p className="bold">{`${data.humidity}%`}</p>
+                  <p>Влажность</p>
+                </div>
+                <div className="wind">
+                  <p className="bold">{`${data.wind}м/с`}</p>
+                  <p>Скорость ветра</p>
+                </div>
+              </div>
+          </div>
+      </div>
+    );
 }
 
-export default App;
+export default App
+
+
